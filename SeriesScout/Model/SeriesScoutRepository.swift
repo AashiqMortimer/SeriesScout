4a2a6574f2f5fce65a3c3cf9d984fdfacfdd7d61
aashiq.mortimer@tui.co.uk <aashiq.mortimer@tui.co.uk>
2024-02-14 16:55:29 +0000

    create model and tests

---
+++ SeriesScout/Model/SeriesScoutRepository.swift
@@ -0,0 +1,8 @@
+//
+//  SeriesScoutRepository.swift
+//  SeriesScout
+//
+//  Created by Aashiq Mortimer on 14/02/2024.
+//
+
+import Foundation
---
+++ SeriesScout/Model/UtellyModel.swift
@@ -0,0 +1,30 @@
+//
+//  UtellyModel.swift
+//  SeriesScout
+//
+//  Created by Aashiq Mortimer on 14/02/2024.
+//
+
+import Foundation
+
+struct UtellyModel: Codable {
+    
+    struct Results: Codable {
+        
+        let locations: [Locations]
+        let picture: String
+        let name: String
+        
+        struct Locations: Codable {
+            let platformDisplayName: String
+            let icon: String
+            
+            enum CodingKeys: String, CodingKey {
+                case platformDisplayName = "display_name"
+                case icon
+            }
+        }
+    }
+    
+    let results: [Results]
+}
