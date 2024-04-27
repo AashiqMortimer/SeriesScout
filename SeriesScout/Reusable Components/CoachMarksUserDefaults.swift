65552597aefd1c8d3931b524900831c3d8b334b7
aashiq.mortimer@tui.co.uk <aashiq.mortimer@tui.co.uk>
2024-04-27 18:00:12 +0100

    Remove commented out code

--- SeriesScout/Reusable Components/CoachMarksUserDefaults.swift
+++ SeriesScout/Reusable Components/CoachMarksUserDefaults.swift
@@ -8,66 +8,6 @@
 import Foundation
 import SwiftUI
 
-//class CoachMarksUserDefaults: ObservableObject {
-//    
-//    //TODO: Try this as a property wrapper again. (Dynamic Property Wrapper)
-//    // This should negate the ViewModel: You would just have this that has the viewCount, interactionOccurred, and shouldShow
-//    // WrappedValue and ProjectedValue.
-//    // Wrapped is a boolean to say if it should show the coach marks or not.
-//    // ProjectedValue would be my userdefaults properties that I have here as a class.
-//    // Would keep this class, alongside the property wrapper.
-//    
-//    
-//    // Ideal implementation within the View:
-//    // Initialise it: @Coachmark(key: "EAT-Shortlist, threshold: 3) var showShortlistCoachMark
-//    // $showShortlistCoachMark.incrementViewCounter()
-//    // $showShortlistCoachMark.markInteractionOcurred()
-//    
-//    private let viewKey: String
-//    private let interactionKey: String
-//    
-//    @Published var interactionOccurred: Bool = false
-//    @Published var viewCount: Int = 0
-//    
-//    private var defaults: UserDefaults
-//    
-//    init(viewKey: String, interactionKey: String, defaults: UserDefaults = .standard) {
-//        self.viewKey = viewKey
-//        self.interactionKey = interactionKey
-//        self.defaults = defaults
-//        
-//        if let coachMarks = defaults.dictionary(forKey: "coachMarks") {
-//            interactionOccurred = coachMarks[interactionKey] as? Bool ?? false
-//            viewCount = coachMarks[viewKey] as? Int ?? 0
-//        }
-//    }
-//    
-//    private func updateDefaults() {
-//        var coachMarks = defaults.dictionary(forKey: "coachMarks") ?? [:]
-//        coachMarks[viewKey] = viewCount
-//        coachMarks[interactionKey] = interactionOccurred
-//        defaults.set(coachMarks, forKey: "coachMarks")
-//    }
-//    
-//    func incrementViewCount() {
-//        viewCount += 1
-//        updateDefaults()
-//    }
-//    
-//    func setInteraction() {
-//        guard !interactionOccurred else { return }
-//        interactionOccurred = true
-//        updateDefaults()
-//    }
-//    
-//    //TODO: Delete later, this is just for testing purposes:
-//    func resetCoachMarks() {
-//        viewCount = 0
-//        interactionOccurred = false
-//        updateDefaults()
-//    }
-//}
-
 class CoachMarksUserDefaults: ObservableObject {
     private let defaults: UserDefaults
     
@@ -136,23 +76,23 @@ class CoachMarksUserDefaults: ObservableObject {
 struct CoachMarkWrapper: DynamicProperty {
     private let keyBase: String
     private let threshold: Int
-    @ObservedObject private var userDefaults: CoachMarksUserDefaults
+    @ObservedObject private var coachMarksDefaults: CoachMarksUserDefaults
     
     var wrappedValue: Bool {
         let viewCountKey = "\(keyBase)_viewCount"
         let interactionKey = "\(keyBase)_interactionFlag"
-        return userDefaults.viewCount(forKey: viewCountKey) >= threshold &&
-               !userDefaults.interactionOccurred(forKey: interactionKey)
+        return coachMarksDefaults.viewCount(forKey: viewCountKey) >= threshold &&
+               !coachMarksDefaults.interactionOccurred(forKey: interactionKey)
     }
     
     var projectedValue: CoachMarksUserDefaults {
-        userDefaults
+        coachMarksDefaults
     }
     
     init(key: String, threshold: Int, userDefaults: CoachMarksUserDefaults = CoachMarksUserDefaults()) {
         self.keyBase = key
         self.threshold = threshold
-        self.userDefaults = userDefaults
+        self.coachMarksDefaults = userDefaults
         userDefaults.initialiseValues(interactionKey: "\(key)_interactionFlag", viewKey: "\(key)_viewCount")
     }
 }
